0f1f9b1dba8970dce315f827299162db3f32ff5d
Dominik Bergermann <dominik.bergermann@rwth-aachen.de>
2026-06-26 10:47:14 +0200

    a

--- testscript.jl
+++ testscript.jl
@@ -24,9 +24,17 @@ end
     return [1]
 end
 
+@get "/echo" function(req::HTTP.Request)
+    display(req.body)
+
+    return "echo"
+end
+
 @get "/rpos/{i}" function(req::HTTP.Request,i::Int)
     return JSON.json(md.logger.rpos[i])
 end
+
+
 
 s = serve(; host="127.0.0.1",port=2000,async=true)
 # HTTP.get("http://127.0.0.1:2000/rpos/1")
