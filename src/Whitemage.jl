8aa134e116f3fe35afb011fbc36db6405adeb8f9
bergermann <dominik.bergermann@gmx.de>
2026-07-29 14:38:31 +0200

    a

--- src/Whitemage.jl
+++ src/Whitemage.jl
@@ -1,7 +1,9 @@
+
 module Whitemage
 
 using Oxygen, HTTP, JSON, Blackmage
 
+include("logger.jl")
 include("server.jl")
 
 function main()
@@ -9,7 +11,14 @@ function main()
 
     addMockLog_(md)
 
-    
+    interrupt = false
+
+    @Threads.spawn begin
+
+    end
+
+
+    startLogger!(md)
 
     s = serve(; host="127.0.0.1",port=2000,async=true)
     # HTTP.get("http://127.0.0.1:2000/rpos/1")
--- src/logger.jl
+++ src/logger.jl
@@ -1,21 +1,35 @@
 
 
-function startLogger!(md::MultiDevice)
+function startLogger!(md::MultiDevice; interval::Real=1.)
     md.logger.active = true
+
+    @info "Starting logger."
     
     if getMeasurementEnabled()
         Threads.@spawn begin
-            runLogger!(md)
+            runLogger!(md,interval)
         end
     end
 end
 
-function runLogger!(md::MultiDevice)
+function runLogger!(md::MultiDevice,interval::Real=1.)
+    @info "Running logger."
+
     while md.logger.active
         lock(md.logger.lock) do
-            updateLog_(md); sleep(1)
+            updateLog_(md); sleep(interval)
         end
     end
+
+    @info "Stopped logger."
+
+    return
+end
+
+function stopLogger!(md::MultiDevice)
+    @info "Stopping logger."
+    
+    md.logger.active = false
 
     return
 end
